c9e3dba760f3a4c996fdb1b03bdb958f5f717169
eddieaviles357 <eduardoaviles357@yahoo.com>
2021-04-13 11:08:00 -0700

    passed clickHandler to Child Avatar component and state

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,41 @@
 import {App_container, background, backgroundAnimated} from '../components/index.module.scss';
 import Avatar from '../components/avatar/Avatar';
 import Categories from '../components/categories/Categories';
-import {getCalculatedPoints, PEC_OBJECTS} from '../components/constants/pecObj';
+import {getCalculatedPoints, categoriesHandler, PEC_OBJECTS} from '../components/constants/pecObj';
 import {useState, useEffect} from 'react';
 
 
 function Home() {
   const [extend, setExtend] = useState(false); // for background animation
-  const [xy, setXY] = useState({x: 0, y: 0});
-  const [xy2, setXY2] = useState({x: 0, y: 0});
-  // const [categoriesXY, setCategoriesXY] = useState({x: 0, y: 0});
+  const [avatarCoord, setAvatarCoord] = useState({x: 0, y: 0});
+  const [categoryCoord, setCategoryCoord] = useState({x: 0, y: 0});
+  
+  useEffect(() => {
+    if(window !== 'undefined') {
+      // center of screen to translate new origin
+      let origin = {
+        x: window.innerWidth / 2,
+        y: window.innerHeight / 2
+      };
+      
+      // point that is going to be rotated ( x2, y2 )
+      let initialPoint = {
+        x: window.innerWidth / 2,
+        y: window.innerHeight / 4
+      }
+      
+      setCategoryCoord( { 
+        x: initialPoint.x, 
+        y: initialPoint.y 
+      } );
 
+      setAvatarCoord( { 
+        x: origin.x, 
+        y: origin.y 
+      } );
+    }
+  }, []);
+  
   const eventHandler = e => {
     const target = e.target,
           tagName = target.tagName.toUpperCase();
@@ -19,92 +44,35 @@ function Home() {
       clickHandler(target);
     }
   };
-
-  const [counter, setCounter] = useState(0);
   
-  useEffect(() => {
-    if(window !== 'undefined') {
-
-      // center of screen to translate new origin
-      let origin = {
-        x: window.innerWidth / 2, y: window.innerHeight / 2
-      };
-
-      // point that is going to be rotated ( x2, y2 )
-      let initialPoint = {
-        x: window.innerWidth / 2,
-        y: window.innerHeight / 4
-      }
-
-      setXY2( { x: initialPoint.x, y: initialPoint.y } );
-      setXY( { x: origin.x, y: origin.y } );
-    }
-  }, []);
-
   return (
     <main 
     onDoubleClick={e=>console.log('x: ', e.clientX, 'y:: ', e.clientY)}
     className={App_container}>
-      <Avatar setXY={setXY}/>
-      {
+      <Avatar setXY={setAvatarCoord}/>
+      { 
         PEC_OBJECTS.map(({ id, img, title, radians }) => {
           return (
             <Categories 
               key={id} 
               title={title} 
               img={img} 
-              x={getCalculatedPoints(xy2, xy,radians).x}
-              y={getCalculatedPoints(xy2, xy,radians).y} 
+              setExtend={setExtend}
+              extend={extend}
+              coord={getCalculatedPoints(categoryCoord, avatarCoord,radians)}
               />
           )
-        })
+        }) 
       }
-        <div 
-          onClick={e=>setExtend(!extend)}
-          className={ 
-            extend ? background : backgroundAnimated
-          }
-          >&nbsp;
-        </div>
+      <div 
+        onClick={e=>setExtend(!extend)}
+        className={ 
+          extend ? background : backgroundAnimated
+        }
+        >&nbsp;
+      </div>
     </main>
   )
 };
-
-
-function clickHandler(elementTarget) {
-  const text = elementTarget.parentElement.parentElement.innerText.toUpperCase();
-      switch(text) {
-        case 'SHIRT': 
-          // setExtend(!extend);
-          console.log('SHIRT element');
-          break;
-        case 'FEELINGS':
-          console.log('FEELINGS element');
-          break;
-        case 'ENTERTAINMENT':
-          console.log("ENTERTAINMENT element");
-          break;
-        case 'HELP':
-          console.log("HELP element");
-          break;
-        case 'HYGIENE':
-          console.log("HYGIENE element");
-          break;
-        case 'FUN':
-          console.log("FUN element");
-          break;
-        case 'SPORTS':
-          console.log("SPORTS element");
-          break;
-        case 'VEGGIES':
-          console.log("VEGGIES element");
-          break;
-        case 'VEGGIES':
-          console.log("VEGGIES element");
-          break;
-        default: 
-          console.log('default hit')
-      }
-    };
 
 export default Home;
